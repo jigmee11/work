36057e1cc17158199dc49264017f5d5dcaa9a36b
Tserenjigmed Shagdar <tserenjigmedshagdar@MacBook-Pro-2.local>
2020-10-14 12:05:34 +0800

    file-promise-finals

--- finals/file-promise.js
+++ finals/file-promise.js
@@ -1,93 +1,35 @@
+const { promises } = require('dns');
 const fs = require('fs');
-const { resolve } = require('path');
-const { versions } = require('process');
+
 const checkWord = w => {
     let letters = /^[A-Za-z]+$/;
-    if (w.match(letters)) return true;
-    return false;
-};  
-// const find = (a)=>{  
-//     return new Promise((res,rej)=>{
-//         a=a.trim();
-//         a = a.split(' ');
-//         let b = [];
-//         a.map(e=>{
-//             if(checkWord(e)){
-//                 e=e.toLowerCase();
-//                 b.push(e);
-//             }
-//         })
-//         res(b);
-//     })
-// }
-const find1 = (a) =>{
-    return new Promise((res,rej)=>{
-        a = a.trim();
-        a = a.split('\n');
-        let b = [];
-        a.map((e,i)=>{
-            e = e.split(' ');
-            e.map(q=>{
-                if(checkWord(q)){
-                    q=q.toLowerCase();
-                    b.push(q);
-                }
-            })
-        })
-        res(b);
-    })
-}
-let word;
-let time=0;
-const main=(a)=>{
-    return new Promise((res,rej)=>{
-        let b = [];
-        let c=true;
-        let j=0;
-        a.forEach((element,i) => {
-            b.forEach(e=>{
-                if(e==element){
-                    c = false;
-                }
-            })
-            if(c){
-                let d = a.slice(i,a.length);
-                let now = d[0];
-                d.map(e=>{
-                    if(e==now){
-                        j++;
-                    }
-                })
-                if(j>time){
-                    word = now;
-                    time = j;
-                }
-                b.push(now);
-            }
-            j=0;
-            c=true;
-        });
-        res([word,time]);
-    })
-}
+    if (w.match(letters)) return w;
+    return;
+}; 
 
-fs.readFile("/Users/tserenjigmedshagdar/Desktop/second-year-Nest/work/big.txt","utf8", (err,data)=> {
-    fs.readFile("/Users/tserenjigmedshagdar/Desktop/second-year-Nest/work/sgb-words.txt","utf8", (err,data1)=> {
-        fs.readFile("/Users/tserenjigmedshagdar/Desktop/second-year-Nest/work/shakespeare.txt","utf8", (err,data2)=> {
-            fs.readFile("/Users/tserenjigmedshagdar/Desktop/second-year-Nest/work/smaller.txt", "utf8",  (err,data3)=> {
-                Promise.all([find1(data),find1(data1),find1(data2),find1(data3)])
-                    .then((result) => {
-                        let k=result[0].concat(result[1],result[2],result[3]);
-                        main(k)
-                        .then((res) => {
-                            console.log(res[0] + " " + res[1]);
-                        }).catch((er) => {
-                            
-                        });
-                    }).catch((err) => {
-                        console.log(err);
-                    });
-            }); 
+const read = (filename) => {
+    return new Promise((res,rej)=>{
+        fs.readFile(`/Users/tserenjigmedshagdar/Desktop/second-year-Nest/work/${filename}`, "utf8",  (err,data)=> {
+            res(data);
         }); 
-    }); 
-}); 
+    })
+}
+Promise.all([read('big.txt'),read('sgb-words.txt'),read('shakespeare.txt'),read('smaller.txt')])
+    .then((result) => {
+        result = result.join().replace(/(\r\n|\n|\r)/gm," ").split(' ').filter(checkWord).map(e=>e.toLowerCase());
+        let count = {};
+        result.map(word=>{
+            if(word in count){
+                count[word]++;
+            }
+            else{
+                count[word]=1;
+            }
+        })
+        let ans = Object.keys(count).sort((a,b)=>{return count[a]-count[b]}).reverse();
+
+        let key = ans.slice(0,10).map(key=>{return {word:key, value: count[key]}});
+        console.log(key);
+    }).catch((err) => {
+        
+    });
